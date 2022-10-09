3283675e3516fe1262cfcc6ef0a1b900496f3003
blessed.chianumba <blessedchianumba@gmail.com>
2022-10-09 21:34:37 +0100

    Added comments

--- get_ground/models/stage/stg_partners.sql
+++ get_ground/models/stage/stg_partners.sql
@@ -7,8 +7,9 @@
 
 SELECT
   CAST(id AS int64) AS id
-  , {{ unix_nano_to_timestamp('created_at') }} AS created_at
-  , {{ unix_nano_to_timestamp('updated_at') }} AS updated_at
+  , {{ unix_nano_to_timestamp('created_at') }} AS created_at -- converting nanoseconds into seconds
+  , {{ unix_nano_to_timestamp('updated_at') }} AS updated_at -- converting nanoseconds into seconds
   , CAST(partner_type AS string) AS partner_type
+  -- Assumption here is that 0 means no sales contact is associated
   , CAST(CASE WHEN lead_sales_contact = '0' THEN NULL ELSE lead_sales_contact END AS string) AS lead_sales_contact
 FROM {{ ref('partners') }}
--- get_ground/models/stage/stg_referrals.sql
+++ get_ground/models/stage/stg_referrals.sql
@@ -7,8 +7,8 @@
 
 SELECT
   CAST(id AS string) AS id
-  , {{ unix_nano_to_timestamp('created_at') }} AS created_at
-  , {{ unix_nano_to_timestamp('updated_at') }} AS updated_at
+  , {{ unix_nano_to_timestamp('created_at') }} AS created_at -- converting nanoseconds into seconds
+  , {{ unix_nano_to_timestamp('updated_at') }} AS updated_at -- converting nanoseconds into seconds
   , CAST(company_id AS int64) AS company_id
   , CAST(partner_id AS int64) AS partner_id
   , CAST(consultant_id AS int64) AS consultant_id
